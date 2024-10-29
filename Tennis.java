d3b759e5aeaecd2229c3b3bc6c75f1dd14dbec30
binf01-Fioretti <105106998+binf01-Fioretti@users.noreply.github.com>
2024-10-29 22:57:32 +0100

    Add files via upload

---
+++ Tennis.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+
+
+/**
+ *
+ * @author B045_12
+ */
+public class Tennis {
+    public String g1;
+    public String g2;
+    public int punti;
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+ 
+    public Tennis(){ 
+    this.g1=g1; 
+    this.g2=g2; 
+    } 
+    public void Tennis(){
+    if(punti==0){ 
+    Partita.yield(); 
+    }else{ 
+    System.out.println("La partita deve ancora iniziare"); 
+    } 
+    }
+}
